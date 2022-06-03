e22e6960cae06932a4de131ae4607c28c28db46a
Franz Noel Tanglao <franz@tanglao.org>
2022-06-03 14:36:59 -0700

    feat: Find median

---
+++ findMedian.ts
@@ -0,0 +1,19 @@
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+  const sortedArrays = [...nums1, ...nums2].sort((a,b) => a-b)
+  const arrayLength = sortedArrays.length
+
+  console.log('arrayLength', arrayLength)
+  if (arrayLength % 2 == 0) {
+    const median = arrayLength/2
+    console.log('median even', median, sortedArrays, sortedArrays[median], sortedArrays[median-1])
+    return (sortedArrays[median] + sortedArrays[median-1]) / 2
+  }
+  
+  const median = Math.ceil(arrayLength/2)
+  console.log('median odd', median-1, sortedArrays, sortedArrays[median-1])
+  return sortedArrays[median-1]
+};
+
+// console.log(findMedianSortedArrays([1,3], [2]))
+// console.log(findMedianSortedArrays([1,2], [3,4]))
+console.log(findMedianSortedArrays([3], [-2,-1]))
